c55c26a4e4ea520cc3cb597c7f34b31bdab77459
Luis <acedilloluis@gmail.com>
2022-12-29 14:24:31 -0800

    added invalid style

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function InputText({ propName, type, label, placeholder, info, setInfo }) {
-  // add styling for required tooltip
   return (
-    <label>
+    <label className="flex flex-wrap items-center justify-between">
       <strong className="text-sm font-normal text-marine-blue">{label}</strong>
+      <span className="invisible text-sm font-normal text-strawberry-red peer-invalid:visible">
+        This field is required
+      </span>
       <input
         type={type}
         name={label}
@@ -17,7 +19,7 @@ function InputText({ propName, type, label, placeholder, info, setInfo }) {
           setInfo({ ...info, [propName]: e.target.value });
         }}
         required="required"
-        className="mb-2 w-full cursor-pointer rounded-md text-base font-medium text-marine-blue placeholder:text-cool-gray focus:border-purplish-blue focus:outline-none focus:ring-0 focus:ring-purplish-blue"
+        className="peer mb-2 mt-1 w-full cursor-pointer rounded-md text-base font-medium text-marine-blue placeholder:text-cool-gray invalid:border-strawberry-red invalid:outline-none invalid:ring-0 invalid:ring-strawberry-red focus:border-purplish-blue focus:outline-none focus:ring-0 focus:ring-purplish-blue"
       />
     </label>
   );
